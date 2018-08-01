49235616caac1543e5127f46e3c4a1241c6ad7c8
Matthieu Brouillard <mbrouilla@gmail.com>
2018-08-02 00:18:49 +0200

    Add some loops

--- main.rb
+++ main.rb
@@ -3,6 +3,29 @@ Project name : Main tests
 Author : brrrouillard
 =end
 
-my_num = 0
-my_bool = false
+first, second = ARGV # Unpacking an array
 
+### LOOPS ###
+
+i = 0
+while i < 4
+    puts "hey"
+    i += 1
+end
+
+for i in 1..5 # .. includes the last number, ... excludes it
+    puts i
+end
+
+### .TIMES {} METHOD
+
+10.times { # Repeat an action x number of times
+    puts "hello"
+} 
+
+### .SPLIT() METHOD
+
+text = "Hello my name is".split(" ") # Passes the delimiter as an argument
+text.each do |word|
+    puts word
+end
