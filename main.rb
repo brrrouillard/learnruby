04271db29c72a06a56e97bf9a0667009db89b3e1
Matthieu Brouillard <mbrouilla@gmail.com>
2018-08-02 10:57:25 +0200

    Add hashes and arrays

--- main.rb
+++ main.rb
@@ -3,9 +3,25 @@ Project name : Main tests
 Author : brrrouillard
 =end
 
-first, second = ARGV # Unpacking an array
+### DATA STRUCTURES
+
+# ARRAYS
+
+my_array = [3, 8]
+first, second = my_array # Unpacking an array
+
+# HASHES
+
+my_hash = Hash.new("default")
+my_hash["Bart"] = "Kid"
+other_hash = {
+    "Homer" => "Adult",
+    "Maggie" => "Baby"
+}
 
 ### LOOPS ###
+
+# WHILE
 
 i = 0
 while i < 4
@@ -13,9 +29,17 @@ while i < 4
     i += 1
 end
 
+# FOR
+
 for i in 1..5 # .. includes the last number, ... excludes it
     puts i
 end
+
+# (FOR) EACH
+
+my_array = ["You", "and", "Me"]
+my_array.each{ |word|
+    puts word}
 
 ### .TIMES {} METHOD
 
